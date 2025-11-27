881767e32126c851de747ea9c8ef95088142ab69
adibrahman <25063087@siswa.um.edu.my>
2025-11-27 20:01:59 +0800

    added: employee file manager file added the jdbc connector file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter User Id: ");
+        String id = sc.nextLine();
+        System.out.print("Enter Password: ");
+        String password = sc.nextLine();
 
-        System.out.println("This is adib");
+
     }
 }
