90ed2ffa06dfd638a9d1af4c7758935287e78772
brandon <brandonbeyonddby@outlook.com>
2025-11-27 22:29:37 +0800

    brandon

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,5 @@ public class Main {
         System.out.ptintln("This is Humam");
         System.out.println("This is Mohammed");
         System.out.println("This is Brandon");
-        System.out.println("hello");
     }
 }
