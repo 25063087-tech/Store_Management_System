f37a30766beda8a309ceb6a812e456f6973feb2a
Yughendraa <25060111@siswa.um.edu.my>
2026-01-17 05:14:48 +0800

    Yughen

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     // Program entry point
     //Loads system data and initializes the login process
-
     public static void main(String[] args) {
         FileManager fm = new FileManager();
         EmployeeManager em = new EmployeeManager();
