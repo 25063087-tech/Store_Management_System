4e7595ee32044c859e7a9ed5015230f4fd4a30e1
Yughendraa <25060111@siswa.um.edu.my>
2026-01-17 14:16:31 +0800

    Yughen

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 // Main entry point of the Store Operations Management System
 // Initializes core managers and starts the application
-
 public class Main {
 
-    // Program entry point
     //Loads system data and initializes the login process
     public static void main(String[] args) {
         FileManager fm = new FileManager();
